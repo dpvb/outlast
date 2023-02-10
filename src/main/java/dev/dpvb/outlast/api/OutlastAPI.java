e9f7b2eb0249b31ae5f9f1153501746047c29190
Matt <58913953+ms5984@users.noreply.github.com>
2023-02-09 21:33:55 -0500

    API * Doc, add impl

--- src/main/java/dev/dpvb/outlast/api/OutlastAPI.java
+++ src/main/java/dev/dpvb/outlast/api/OutlastAPI.java
@@ -1,12 +1,29 @@
 package dev.dpvb.outlast.api;
 
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.function.Consumer;
+
+@ApiStatus.NonExtendable
 public interface OutlastAPI {
+    /**
+     * Performs an action with the Outlast plugin instance.
+     *
+     * @param action the action to perform
+     */
+    void withPlugin(@NotNull Consumer<JavaPlugin> action);
     // as needed
 
-    static @NotNull OutlastAPI getInstance() {
+    /**
+     * Gets the API instance.
+     *
+     * @return the Outlast API instance
+     * @throws IllegalStateException if the plugin is not loaded
+     */
+    static @NotNull OutlastAPI getInstance() throws IllegalStateException {
         final var api = Bukkit.getServicesManager().load(OutlastAPI.class);
         if (api == null) {
             throw new IllegalStateException("OutlastAPI is not initialized");
--- src/main/java/dev/dpvb/outlast/internal/OutlastPlugin.java
+++ src/main/java/dev/dpvb/outlast/internal/OutlastPlugin.java
@@ -3,6 +3,9 @@ package dev.dpvb.outlast.internal;
 import dev.dpvb.outlast.api.OutlastAPI;
 import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Consumer;
 
 public class OutlastPlugin extends JavaPlugin implements OutlastAPI {
     @Override
@@ -15,5 +18,10 @@ public class OutlastPlugin extends JavaPlugin implements OutlastAPI {
     public void onDisable() {
         // Unregister the API
         getServer().getServicesManager().unregister(OutlastAPI.class, this);
+    }
+
+    @Override
+    public void withPlugin(@NotNull Consumer<JavaPlugin> action) {
+        action.accept(this);
     }
 }
