ae6ab0710685227b4a1d6a3a8428a1a51cd2acfc
Matt <58913953+ms5984@users.noreply.github.com>
2023-02-10 21:30:37 -0500

    Separate API impl; doc

--- src/main/java/dev/dpvb/outlast/internal/OutlastPlugin.java
+++ src/main/java/dev/dpvb/outlast/internal/OutlastPlugin.java
@@ -3,55 +3,19 @@ package dev.dpvb.outlast.internal;
 import dev.dpvb.outlast.api.OutlastAPI;
 import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.NotNull;
 
-import java.util.HashMap;
-import java.util.function.Consumer;
-
-public class OutlastPlugin extends JavaPlugin implements OutlastAPI {
-    private final HashMap<Class<?>, Object> services = new HashMap<>();
+public class OutlastPlugin extends JavaPlugin {
+    final OutlastAPIImpl api = new OutlastAPIImpl();
 
     @Override
     public void onEnable() {
         // Register the API
-        getServer().getServicesManager().register(OutlastAPI.class, this, this, ServicePriority.Normal);
+        getServer().getServicesManager().register(OutlastAPI.class, api, this, ServicePriority.Normal);
     }
 
     @Override
     public void onDisable() {
         // Unregister the API
-        getServer().getServicesManager().unregister(OutlastAPI.class, this);
-    }
-
-    @Override
-    public void withPlugin(@NotNull Consumer<JavaPlugin> action) {
-        action.accept(this);
-    }
-
-    @Override
-    public <T> void withServiceGraceful(@NotNull Class<T> service, @NotNull Consumer<T> action) {
-        synchronized (services) {
-            final var instance = services.get(service);
-            if (instance != null) {
-                action.accept(service.cast(instance));
-            }
-        }
-    }
-
-    @Override
-    public <T> void withService(@NotNull Class<T> service, @NotNull Consumer<T> action) {
-        synchronized (services) {
-            final var instance = services.get(service);
-            if (instance == null) {
-                throw new IllegalStateException("Service " + service.getName() + " is not available");
-            }
-            action.accept(service.cast(instance));
-        }
-    }
-
-    public <T> void putService(Class<T> service, T instance) {
-        synchronized (services) {
-            services.put(service, instance);
-        }
+        getServer().getServicesManager().unregister(OutlastAPI.class, api);
     }
 }
--- src/main/java/dev/dpvb/outlast/internal/package-info.java
+++ src/main/java/dev/dpvb/outlast/internal/package-info.java
@@ -1,5 +1,9 @@
 /**
  * Plugin internals.
+ * <p>
+ * These classes represent core implementation of the plugin and should not
+ * be used from other packages. Systems are encouraged to define API in their
+ * own packages and register with the {@link dev.dpvb.outlast.api.OutlastAPI}.
  */
 @ApiStatus.Internal
 package dev.dpvb.outlast.internal;
