6dd1d641283a8add710895d293c8afcadf219e9d
Matt <58913953+ms5984@users.noreply.github.com>
2023-02-10 23:17:06 -0500

    equals+hashCode, toString

--- src/main/java/dev/dpvb/outlast/sql/SQLPlayer.java
+++ src/main/java/dev/dpvb/outlast/sql/SQLPlayer.java
@@ -3,6 +3,7 @@ package dev.dpvb.outlast.sql;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Objects;
 import java.util.UUID;
 
 public final class SQLPlayer {
@@ -50,5 +51,40 @@ public final class SQLPlayer {
 
     public void setTeam(@Nullable String team) {
         this.team = team;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        SQLPlayer sqlPlayer = (SQLPlayer) o;
+
+        if (kills != sqlPlayer.kills) return false;
+        if (deaths != sqlPlayer.deaths) return false;
+        if (coins != sqlPlayer.coins) return false;
+        if (!uuid.equals(sqlPlayer.uuid)) return false;
+        return Objects.equals(team, sqlPlayer.team);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = uuid.hashCode();
+        result = 31 * result + (int) kills;
+        result = 31 * result + (int) deaths;
+        result = 31 * result + coins;
+        result = 31 * result + (team != null ? team.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "SQLPlayer{" +
+                "uuid=" + uuid +
+                ", kills=" + kills +
+                ", deaths=" + deaths +
+                ", coins=" + coins +
+                ", team='" + team + '\'' +
+                '}';
     }
 }
