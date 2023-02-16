a0fe82f001c685af71b3d2fd31f7d190da5751e0
dpvb <bungohdev@gmail.com>
2023-02-15 22:09:24 -0500

    Implemented /team info and /team leave

--- src/main/java/dev/dpvb/outlast/internal/Commands.java
+++ src/main/java/dev/dpvb/outlast/internal/Commands.java
@@ -11,9 +11,12 @@ import dev.dpvb.outlast.sql.cache.LocationCache;
 import dev.dpvb.outlast.sql.cache.PlayerCache;
 import dev.dpvb.outlast.sql.cache.TeamCache;
 import dev.dpvb.outlast.sql.models.SQLLocation;
+import dev.dpvb.outlast.sql.models.SQLPlayer;
 import dev.dpvb.outlast.sql.models.SQLTeam;
 import dev.dpvb.outlast.teleportation.TeleportRequest;
 import dev.dpvb.outlast.teleportation.TeleportService;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
@@ -21,10 +24,21 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 class Commands {
 
-    Commands() {}
+    private final TeamCache teamCache;
+    private final PlayerCache playerCache;
+    private final LocationCache locationCache;
+
+    Commands() {
+        final SQLService sql = SQLService.getInstance();
+        teamCache = sql.getTeamCache();
+        playerCache = sql.getPlayerCache();
+        locationCache = sql.getLocationCache();
+    }
 
     public void initCommands(Plugin plugin) throws Exception {
         final PaperCommandManager<CommandSender> manager = PaperCommandManager.createNative(plugin, CommandExecutionCoordinator.simpleCoordinator());
@@ -85,13 +99,11 @@ class Commands {
     public void createTeam(CommandSender sender, @NotNull @Argument("name") @Regex(".{1,30}") String name) {
         // TODO do we need a regex check? not sure how it works.
         final Player player = (Player) sender;
-        final PlayerCache playerCache = SQLService.getInstance().getPlayerCache();
         // check if this player is already in a team
         if (playerCache.getModel(player.getUniqueId()).getTeam_name() != null) {
             player.sendPlainMessage("You are already on a team.");
             return;
         }
-        final TeamCache teamCache = SQLService.getInstance().getTeamCache();
         // check if team with this name already exists
         SQLTeam checkTeam = teamCache.getModel(name);
         if (checkTeam != null) {
@@ -113,12 +125,39 @@ class Commands {
     @CommandMethod(value = "team join <name>", requiredSender = Player.class)
     @CommandDescription("Joins a team with the given name")
     public void joinTeam(CommandSender sender, @NotNull @Argument("name") @Regex(".{1,30}") String name) {
-        //test
+
     }
 
     @CommandMethod(value = "team info [name]", requiredSender = Player.class)
     @CommandDescription("Displays information about your team or the given team name")
     public void getTeamInfo(CommandSender sender, @Nullable @Argument("name") @Regex(".{1,30}") String name) {
+        final Player player = (Player) sender;
+        // Get the name of the team you want info on.
+        if (name == null) {
+            name = playerCache.getModel(player.getUniqueId()).getTeam_name();
+            if (name == null) {
+                player.sendPlainMessage("You are not on a team!");
+                return;
+            }
+        } else {
+            SQLTeam team = teamCache.getModel(name);
+            if (team == null) {
+                player.sendPlainMessage("That team does not exist.");
+                return;
+            }
+        }
+
+        final String teamName = name;
+        OfflinePlayer teamLeader = Bukkit.getOfflinePlayer(teamCache.getModel(teamName).getLeader());
+        List<OfflinePlayer> otherMembers = playerCache.getModels().stream()
+                .filter(sqlPlayer -> teamName.equals(sqlPlayer.getTeam_name()))
+                .map(sqlPlayer -> Bukkit.getOfflinePlayer(sqlPlayer.getPlayer_uuid()))
+                .toList();
+        player.sendPlainMessage("Team " + name + ":");
+        player.sendPlainMessage(teamLeader.getName() + " (Leader)");
+        for (OfflinePlayer offlinePlayer : otherMembers) {
+            player.sendPlainMessage(offlinePlayer.getName() + "");
+        }
     }
 
     @CommandMethod(value = "team userinfo <player>", requiredSender = Player.class)
@@ -128,7 +167,41 @@ class Commands {
 
     @CommandMethod(value = "team leave", requiredSender = Player.class)
     @CommandDescription("Leave your team")
-    public void createTeam(CommandSender sender) {
+    public void leaveTeam(CommandSender sender) {
+        // check if player is on a team
+        final Player player = (Player) sender;
+        String teamName = playerCache.getModel(player.getUniqueId()).getTeam_name();
+        if (teamName == null) {
+            player.sendPlainMessage("You are not in a team!");
+            return;
+        }
+        // retrieve your teammates
+        List<UUID> teamMates = playerCache.getModels().stream()
+                .filter(sqlPlayer -> teamName.equals(sqlPlayer.getTeam_name()))
+                .map(SQLPlayer::getPlayer_uuid)
+                .filter(uuid -> !uuid.equals(player.getUniqueId()))
+                .toList();
+
+        // remove the player from the team
+        playerCache.updateModel(player.getUniqueId(), sqlPlayer -> {
+            sqlPlayer.setTeam_name(null);
+        });
+
+        // if you have 0 teammates, delete the team
+        if (teamMates.size() == 0) {
+            teamCache.deleteModel(teamName);
+        } else {
+            // check if the team leader is the player who is leaving.
+            UUID teamLeader = teamCache.getModel(teamName).getLeader();
+            if (teamLeader.equals(player.getUniqueId())) {
+                // need to give team leader to someone else.
+                teamCache.updateModel(teamName, sqlTeam -> {
+                    sqlTeam.setLeader(teamMates.get(0));
+                });
+            }
+        }
+
+        player.sendPlainMessage("You left the team.");
     }
 
     @CommandMethod(value = "team setleader <player>", requiredSender = Player.class)
@@ -178,7 +251,6 @@ class Commands {
     @CommandPermission("outlast.admin")
     public void setSpawn(CommandSender sender) {
         final Player player = (Player) sender;
-        final LocationCache locationCache = SQLService.getInstance().getLocationCache();
         final SQLLocation spawn = locationCache.getModel("spawn");
         if (spawn == null) {
             locationCache.createModel("spawn", loc -> {
